13ac0b990a558a2fca136faace29c81dd102a6c4
Doug Durdin <ddurdin@gmail.com>
2022-02-16 15:40:37 -0500

    Format temerature

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ class Pi(MycroftSkill):
           value = s.decode('UTF-8')
           a = len(value)
           self.log.info(f'Length of temperature: {a}')
-          value = value[:(a-3)] + "." + value[-3:]
+          value = value[:(a-4)] + "." + value[-4:]
 
         if property == 'operating system':
           name = 'operating system'
