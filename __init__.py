d0c634479caa4a2ffbcadbcd2cee84820b40d22d
Doug Durdin <ddurdin@gmail.com>
2022-02-16 15:17:06 -0500

    Format the temperature

--- __init__.py
+++ __init__.py
@@ -30,6 +30,7 @@ class Pi(MycroftSkill):
           s = subprocess.check_output(["cat", "/sys/class/thermal/thermal_zone0/temp"])
           value = s.decode('UTF-8')
           a = len(value)
+          self.log.info(f'Length of temperature: {a}')
           value = value[:(a-2)] + "." + value[2:]
 
         if property == 'operating system':
